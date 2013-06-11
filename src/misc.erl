9d48f4e79f0e40dca2d01a6058ab8196ba01e7f6
root <root@localhost.localdomain>
2013-06-11 14:23:50 -0400

    able to connect to rabbitmq server

--- src/misc.erl
+++ src/misc.erl
@@ -2,15 +2,30 @@
 
 -include("amqp_client.hrl").
 
--export([declare_exchanges/1,
+-export([setup_connection/0,
+	 declare_exchanges/1,
          demo_callback/2,
          word_count_callback/2,
          word_reverse_callback/2,
          publish_msg/3]).
 
-declare_exchanges(Exchanges) ->
-    {ok, Connection} = amqp_connection:start(#amqp_params_network{}),
+
+get_connection_params() ->
+    #amqp_params_network{
+		 username = <<"guest">>,
+		 password = <<"guest">>,
+                 virtual_host = <<"/">>,
+		 host = "192.168.4.128"}.
+
+setup_connection()->
+    Amqp_params_network = get_connection_params(),
+    {ok, Connection} = amqp_connection:start(Amqp_params_network),
     {ok, Channel} = amqp_connection:open_channel(Connection),
+    {ok, {Connection, Channel}}.
+
+declare_exchanges(Exchanges) ->
+    {ok, {Connection, Channel}} = setup_connection(),
+
 
     [ #'exchange.declare_ok'{} = amqp_channel:call(Channel,
                                 #'exchange.declare'{ exchange = Name,
